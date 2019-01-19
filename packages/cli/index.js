e27f4e1b86a7989ebc782baf69f13fe7b20fbbb4
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-01-19 08:10:55 +0000

    Add @jcc/cli

---
+++ packages/cli/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const jcc = require( "@jcc/core" );
+
+/**
+ * Execute the JCC command line interface
+ * 
+ * @param {string} _cwd The currently working directory
+ * @param {string[]} _args Arguments passed from the CLI tool
+ */
+
+async function main( _cwd, _args ) {
+
+    // TODO...
+
+}
+
+module.exports = main;
